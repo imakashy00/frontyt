eaa34d11fbbfe7dc4845084fe904167ee49f6eba
Akash Yadav <imakashy00@gmail.com>
2025-03-11 14:23:44 +0530

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-// const publicRoutes = ["/", "/terms", "/privacy-policy"];
+const publicRoutes = ["/", "/terms", "/privacy-policy"];
 export default function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  
+
   const accessToken = req.cookies.get("access_token")?.value || null;
+  const refreshToken = req.cookies.get("refresh_token")?.value || null;
+  if (
+    publicRoutes.some((route) => path === route || path.startsWith(`${route}/`))
+  ) {
+    return NextResponse.next();
+  }
 
   const isProtectedRoute = path.startsWith("/dashboard");
 
   // Redirect unauthenticated users from protected routes
-  if (isProtectedRoute && !accessToken) {
+  if (isProtectedRoute && !accessToken && !refreshToken) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
@@ -22,5 +28,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
